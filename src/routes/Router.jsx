7275aa56b508d95094b31b9ca859de38ffb428b0
achmal <achmalpradiptaaditama@gmail.com>
2024-03-08 22:24:18 +0700

    A

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoutes from '../components/routes/PrivateRoute'
 
 // pages
 import AuthPage from '../pages/AuthPage';
